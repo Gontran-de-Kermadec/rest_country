7234f0965405e3c9b9f642520f843b3b89bf0d78
Gontran de Kermadec <gontrandekermadec@gmail.com>
2022-04-18 17:28:21 +0200

    profile component css added

--- src/components/Profile.vue
+++ src/components/Profile.vue
@@ -1,54 +1,70 @@
 <template>
 	<div>
-		<h1>{{ username }}</h1>
-		<button @click="logOut">Logout</button>
-		<p>Delete account</p>
-		<p>All your notes</p>
-		<div
-			v-for="(country, key) in allUserNotes"
-			:key="country.id"
-			:data-country-id="country.id"
-			class="notes__container"
-		>
-			<h2>{{ key }}</h2>
-			<router-link
-				:to="{
-					name: 'Country',
-					params: {
-						region: country[0].region,
-						country: key,
-					},
-				}"
-			>
-				<p>Check page</p>
-			</router-link>
+		<header class="profile__header">
+			<router-link :to="{ name: 'Homepage' }">Homepage</router-link>
+			<p>Hi {{ username }} !</p>
+			<button @click="logOut">Logout</button>
+		</header>
+		<section class="profile__resume">
+			<p>Visited countries: x</p>
+			<p>To visit countries: x</p>
+			<p>Delete account</p>
+			<p>Update profile</p>
+		</section>
+		<h1>Your notes</h1>
+		<div class="allnotes__container">
 			<div
-				v-for="note in country"
-				:key="note.id"
-				:data-note-id="note.id"
-				class="single__note"
+				v-for="(country, key) in allUserNotes"
+				:key="country.id"
+				:data-country-id="country.id"
+				class="notes__container"
 			>
-				<div>
-					<h3>Location</h3>
-					<p>{{ note.location }}</p>
+				<div class="note__country">
+					<h2>{{ key }}</h2>
+					<router-link
+						:to="{
+							name: 'Country',
+							params: {
+								region: country[0].region,
+								country: key,
+							},
+						}"
+						class="note__country--link"
+					>
+						Check page
+					</router-link>
 				</div>
-				<div>
-					<h3>Content</h3>
-					<p>{{ note.content }}</p>
-				</div>
-				<div @click="deleteNote">X</div>
-				<!-- <router-link
-					:to="{
-						name: 'Country',
-						params: {
-							region: note.region,
-							country: key,
-						},
-						hash: '#' + note.id,
-					}"
+				<!-- <div
+					v-for="note in country"
+					:key="note.id"
+					:data-note-id="note.id"
+					class="single__note"
 				>
-					<p>Check page</p>
-				</router-link> -->
+					<div>
+						<h3>Location</h3>
+						<p>{{ note.location }}</p>
+					</div>
+					<div>
+						<h3>Content</h3>
+						<p>{{ note.content }}</p>
+					</div>
+					<div @click="deleteNote" class="single__note--delete">X</div>
+				</div> -->
+				<table>
+					<thead>
+						<tr>
+							<th>Location</th>
+							<th>Content</th>
+						</tr>
+					</thead>
+					<tbody>
+						<tr v-for="note in country" :key="note.id" :data-note-id="note.id">
+							<td>{{ note.location }}</td>
+							<td>{{ note.content }}</td>
+							<td @click="deleteNote" class="single__note--delete">X</td>
+						</tr>
+					</tbody>
+				</table>
 			</div>
 		</div>
 	</div>
@@ -84,21 +100,6 @@
 			username: (state) => state.username,
 			userId: (state) => state.userId,
 		}),
-		// test: function () {
-		// 	const filteredArr = this.allUserNotes.reduce((acc, obj) => {
-		// 		//target[index] = key;
-		// 		const cle = obj["country"];
-		// 		console.log(acc);
-		// 		console.log(cle);
-		// 		//console.log(target.country, key.country, index);
-		// 		if (!acc[cle]) {
-		// 			acc[cle] = [];
-		// 		}
-		// 		acc[cle].push(obj);
-		// 		return acc;
-		// 	}, {});
-		// 	return filteredArr;
-		// },
 		methods: {
 			async getAllUserNotes() {
 				this.allUserNotes = [];
@@ -172,15 +173,69 @@
 		beforeMount() {
 			this.getAllUserNotes();
 		},
-		mounted() {
-			//this.testOnArr();
-		},
+		mounted() {},
 	};
 </script>
 
 <style scoped>
-	.single__note {
+	.profile__header {
 		display: flex;
-		justify-content: space-around;
+		justify-content: space-between;
+	}
+	.profile__header a,
+	.profile__header button {
+		all: unset;
+	}
+	.allnotes__container {
+		margin: 0 5em;
+	}
+	.note__country {
+		display: flex;
+		justify-content: space-between;
+		margin: 2em 0;
+	}
+	h2 {
+		text-align: left;
+		text-transform: capitalize;
+	}
+	/* .single__note {
+		display: flex;
+		justify-content: space-between;
+	} */
+	.note__country--link {
+		align-self: center;
+		color: lightgray;
+		text-decoration: none;
+		/* margin: 0 2em; */
+	}
+	table {
+		all: unset;
+		border-collapse: collapse;
+		table-layout: fixed;
+		width: 100%;
+	}
+	thead {
+		border-bottom: solid;
+	}
+	thead th {
+		width: 30vw;
+		padding: 1em;
+	}
+	tbody tr {
+		border-top: solid 1px;
+	}
+	tbody td {
+		padding: 1em;
+		word-break: break-all;
+	}
+	.single__note--delete {
+		transform: translateX(40px);
+		opacity: 0;
+		cursor: pointer;
+		transition: all 0.5s;
+	}
+	tbody tr:hover .single__note--delete {
+		opacity: 1;
+		transition: all 0.5s;
 	}
 </style>
